cead8b84a15b1a69ce30cc357fa357425765f829
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-01-23 22:56:08 -0600

    Update CommonSubstring.java
    Updated the file to meet question assignment requirements.

--- src/CommonSubstring.java
+++ src/CommonSubstring.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+//Very similar to CommonSubsequence algorithm
+
 public class CommonSubstring {
-    public static void main(String[] args) {
-        String text1 = "spy family";
-        String text2 = "jujutzu";
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter sequence 1: ");
+        String text1 = scanner.nextLine();
+
+        System.out.println("Enter sequence 2:");
+        String text2 = scanner.nextLine();
+
+        int subSeqLen = 0;
+        String subStringText = "";
+
+        for(int i = 0; i < text1.length(); i++){
+            for(int j = 0; j < text2.length(); j++){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    subSeqLen++;
+                    subStringText = " "+ text1.charAt(i);
+                } else if (subStringText == " ") {
+                    subStringText = "";
+                }
+            }
+        }
+
+        System.out.println("The longest Common subsequence length is " + subSeqLen);
+        System.out.println("The string sequence is " + subStringText);
 
 
     }
