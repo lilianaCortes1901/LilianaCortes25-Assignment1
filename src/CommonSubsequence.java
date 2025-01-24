1a2b8058f53e18c05458f3f0b51419aacf2c3a52
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-01-23 20:44:47 -0600

    Finished updating CommonSubsequence.java file to satisfy question requirements

--- src/CommonSubsequence.java
+++ src/CommonSubsequence.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
 public class CommonSubsequence {
-    public static void main(String[] args) {
-        System.out.println("Enter sequence 1: ");
-        Scanner userInput1 = new Scanner(System.in); //T(1), S(1)
-        System.out.println("Enter sequence 2: ");
-        Scanner userInput2 = new Scanner(System.in);
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
 
-        String [] text1 = [userInput1];
-        String [] text2 = new String[userInput2];
+        System.out.println("Enter sequence 1: ");
+        String text1 = scanner.nextLine();
+
+        System.out.println("Enter sequence 2:");
+        String text2 = scanner.nextLine();
+
         int subSeqLen = 0;
 
-        for (int i = 0; i < text1.length; i++) {
-            for (int j = 0; j < text2.length; j++){
-                if (text1[i] == text2[j]){
+        for(int i = 0; i < text1.length(); i++){
+            for(int j = 0; j < text2.length(); j++){
+                if(text1.charAt(i) == text2.charAt(j)){
                     subSeqLen++;
                 }
             }
         }
 
-        System.out.println("The longest common subsequence length is " + subSeqLen);
+        System.out.println("The longest Common subsequence length is " + subSeqLen);
+
 
     }
 }
