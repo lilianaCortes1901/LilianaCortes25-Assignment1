b3af611beceae67fa0e9afc840b78c699db85c50
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-01-25 21:34:25 -0600

    Finished Assignment
    Finished updating all assignment requirements

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -1,6 +1,15 @@
 
 public class RemoveElement {
-    public static void main(String [] args){
-      
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int foundValNums = 0; //create space in memory to hold numbers in array not equal to val
+            for(int i = 0; i < nums.length; i++){ //O(n), n = nums.length, s(1), creating memory for int i
+                if(nums[i] != val){ //if elements don't equal to val
+                    nums[foundValNums] = nums[i]; //reassign those elements to the nums array
+                    foundValNums++; //create space for more elements
+                }
+            }
+            return foundValNums; //return new list of numbers
+        }
     }
 }
--- src/WhereInSequence.java
+++ src/WhereInSequence.java
@@ -6,13 +6,15 @@ public class WhereInSequence {
 
         System.out.println("Enter integer to search for: ");
         int targetNum = scanner.nextInt();
+        long targetPosition =0;
 
         long [] notFibbSeq = {0, 1};
         for (int i = 2; i < notFibbSeq.length; i++){
             notFibbSeq[i] =  (notFibbSeq[i-1] * 3) + (notFibbSeq[i-2] * 2);
             if (targetNum == notFibbSeq[i]){
-                System.out.println(notFibbSeq[i]);
-            } else if(targetNum != notFibbSeq[i] && notFibbSeq[i] < targetNum){
+                targetPosition = notFibbSeq[i];
+                System.out.println(targetPosition);
+            } else if(targetNum != notFibbSeq[i] && notFibbSeq[i-1] < targetNum){
                 System.out.println(notFibbSeq[i]);
             }
         }
